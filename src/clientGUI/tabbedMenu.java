fc51be7d7895fcb595641e353c8222c08fecb3d2
bahafeld <bahafeld@outlook.com>
2016-03-16 14:49:54 +0100

    -Added tabs to employeeview

--- src/clientGUI/tabbedMenu.java
+++ src/clientGUI/tabbedMenu.java
@@ -95,7 +95,7 @@ public class tabbedMenu extends JFrame {
     private class employeeTab extends JPanel {
         //String[][] table = {{ "Bob", "0" }, { "John", "1" }, { "Dave", "3" }}; //TESTING
 		String[][] table = sql.getStringTable("SELECT user_name, user_email, user_adress, user_postnr, user_tlf FROM HCL_users", false);
-		String[] titles = { "Employees", "E-mail", "Adress", "Zip-Code", "Number"};
+		String[] titles = { "Employees", "E-mail", "Adress ", "Zip-Code", "Number"};
         public employeeTab() {
             setLayout(new BorderLayout());
 			//add(new top(), BorderLayout.NORTH);
