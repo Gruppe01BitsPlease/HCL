09ff41e44a98ad55747c55cf478c3ec04a8f1ceb
jenstk <jenstk@student.hist.no>
2016-03-17 10:43:44 +0100

    stuff

--- src/clientGUI/tabbedMenu.java
+++ src/clientGUI/tabbedMenu.java
@@ -49,7 +49,7 @@ public class tabbedMenu extends JFrame {
         String[] titles;
         JTable list;
         int type;
-        public genericList(String[][] table, String[] titles, int type) {
+        public genericList(String[][] table, String[] titles, int type, boolean searchable) {
             this.table = table;
             this.titles = titles;
             this.type = type;
@@ -77,7 +77,9 @@ public class tabbedMenu extends JFrame {
             });
             JScrollPane scroll = new JScrollPane(list);
             add(scroll, BorderLayout.CENTER);
-            add(new genericSearch(), BorderLayout.SOUTH);
+			if (searchable) {
+				add(new genericSearch(), BorderLayout.SOUTH);
+			}
         }
         private class orderWindow extends JFrame {
             public orderWindow(String[] selected, int index) {
@@ -205,7 +207,8 @@ public class tabbedMenu extends JFrame {
                 public searchWindow() {
                     setSize((int) (x * 0.4), (int) (y * 0.4));
                     setTitle("Search results");
-                    genericList searchTab = new genericList(searchTable, titles, type);
+					setAlwaysOnTop(true);
+                    genericList searchTab = new genericList(searchTable, titles, type, false);
                     add(searchTab, BorderLayout.CENTER);
                     setLocationRelativeTo(null);
                     setVisible(true);
@@ -272,13 +275,13 @@ public class tabbedMenu extends JFrame {
 	}
     //Tabs for the menu, to add one just add it to "tabs" above
     private class employeeTab extends JPanel {
-        String[][] table = {{ "Bob", "bob@bob.com", "0" }, { "John", "John.com", "1" }, { "Dave", "Dave.com", "3" }}; //TESTING
-//		String[][] table = sql.getStringTable("SELECT user_name, user_epost, user_adresse FROM HCL_users", false);
+    //    String[][] table = {{ "Bob", "bob@bob.com", "0" }, { "John", "John.com", "1" }, { "Dave", "Dave.com", "3" }}; //TESTING
+		String[][] table = sql.getStringTable("SELECT last_name, email, adress FROM HCL_employee", false);
 		String[] titles = { "Employees", "E-mail", "Address" };
         JTable list;
         public employeeTab() {
             setLayout(new BorderLayout());
-            add(new genericList(table, titles, 1), BorderLayout.CENTER);
+            add(new genericList(table, titles, 1, true), BorderLayout.CENTER);
         }
     }
     private class CEOtab extends JPanel {
@@ -295,7 +298,7 @@ public class tabbedMenu extends JFrame {
 		String[] titles = { "Customer", "Adress"};
 		public orderTab() {
 			setLayout(new BorderLayout());
-			add(new genericList(table, titles, 2), BorderLayout.CENTER);
+			add(new genericList(table, titles, 2, true), BorderLayout.CENTER);
 		}
 	}
 }
