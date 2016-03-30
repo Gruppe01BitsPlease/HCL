571d03ce3cb92acd3983f02ac36605db3b89be02
bahafeld <bahafeld@outlook.com>
2016-03-30 10:46:31 +0200

    JUNIT

---
+++ src/test/backend/CustomerManagerTest.java
@@ -0,0 +1,38 @@
+package backend;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bahafeld on 30.03.2016...
+ */
+public class CustomerManagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void generate() throws Exception {
+
+    }
+
+    @Test
+    public void edit() throws Exception {
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+}
