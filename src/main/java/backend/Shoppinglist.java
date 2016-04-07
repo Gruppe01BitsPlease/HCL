b875e0bf1b4b6ecaa08861c201740a03938efcc4
Olavhus <OlavH96@gmail.com>
2016-04-07 13:15:09 +0200

    Finished Shoppinglist

--- src/main/java/backend/Shoppinglist.java
+++ src/main/java/backend/Shoppinglist.java
@@ -1,11 +1,9 @@
 package backend;
 
 import javax.xml.transform.Result;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Faiter119 on 01.04.2016.
@@ -21,59 +19,64 @@ public class Shoppinglist {
     /**
      * @return Shopping list as String[]
      * @param interval Days from now until end of shopping list
-     * TODO: Make from scratch with prepared statements..
      */
     public String[][] getShoppinglist(int interval){
 
-        String prepString = "";
+        String prepString = "SELECT * FROM ingredients_to_buy_over_zero WHERE delivery_date BETWEEN CURDATE() AND CURDATE() + INTERVAL ? DAY;";
         ArrayList<ArrayList<String>> list = new ArrayList<ArrayList<String>>();
 
         try {
             PreparedStatement prep = sql.connection.prepareStatement(prepString);
             prep.setInt(1,interval);
 
+            System.out.println(prep.toString());
+
             ResultSet res = prep.executeQuery();
             ResultSetMetaData meta = res.getMetaData();
 
-            int row = -1;
+            int row = 0;
             int colomns = meta.getColumnCount();
 
             while(res.next()){
                 row++;
-                for(int i = 0; i<colomns; i++) {
-                    list.get(row).add(res.getString(i));
+                list.add(new ArrayList<String>());
+
+                for(int i = 1; i<=colomns; i++) {
+                    list.get(row-1).add(res.getString(i));
                 }
             }
 
+            String[][] out = new String[list.size()][];
 
-            String[][] out = new String[colomns][row];
-
-            for(int i=0; i<list.size(); i++){
-
-                for(int j=0; j<colomns; j++){
-
-                    out[i][j] = list.get(i).get(j);
-
-                }
-
+            for (int i = 0; i < list.size(); i++) {
+                ArrayList<String> rad = list.get(i);
+                out[i] = rad.toArray(new String[rad.size()]);
             }
             return out;
         }
-        catch (SQLException e){return null;}
+        catch (SQLException e){
+            return null;
+        }
     }
 
 
     public static void main(String[]args){
 
-        //
-        int lowestStock = 5;
 
         SQL sql = new SQL();
+        Shoppinglist list = new Shoppinglist();
 
-        String[][] list = sql.getStringTable("Select * from HCL_food NATURAL JOIN HCL_food_ingredient NATURAL JOIN " +
-                "HCL_ingredient", false);
+        String[][] array = list.getShoppinglist(365);
 
-        sql.print2dArray(list);
+        System.out.println(Arrays.deepToString(array));
+
+        for(String[] row : array){
+            System.out.println();
+            for(String colomn : row){
+                System.out.print(colomn+" ");
+            }
+
+        }
 
     }
 
