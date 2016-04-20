013486a1ef02ccbc0b41a9b7f542a56321f9d254
bahafeld <bahafeld@outlook.com>
2016-04-20 11:45:20 +0200

    -- Touchup on databaseresetter to make more userfriendly and a bit more error handeling

--- src/main/java/clientGUI/DatabaseResetGUI.java
+++ src/main/java/clientGUI/DatabaseResetGUI.java
@@ -4,13 +4,10 @@ import backend.*;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.FileNotFoundException;
+
 import backend.SQL;
-import backend.SettingsFile;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
 
@@ -22,28 +19,31 @@ import java.sql.SQLException;
 //TODO Edit filereference before deployement
     public class DatabaseResetGUI extends JDialog{
     private JCheckBox checkTables;
-    private JCheckBox checkViews;
-    private JCheckBox checkData;
+    private JCheckBox checkRandomData;
     private JCheckBox checkOverWriteTables;
-    private DatabaseReset reset;
+    private SQLScriptReader reset;
     private JTextPane helpText;
 
 
     public DatabaseResetGUI(){
-        reset = new DatabaseReset();
+        reset = new SQLScriptReader();
         setVisible(true);
 
             //window parameters
-            setLayout(new GridLayout(6, 1));
+            setTitle("Database Setup");
+            setLayout(new GridLayout(5, 1));
             Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
             double x = (double) screen.width * 0.25;
             double y = (double) screen.height * 0.6;
             setSize((int) x, (int) y);
+            setResizable(false);
+            setModalityType(ModalityType.APPLICATION_MODAL);
+            setAlwaysOnTop(true);
+            setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
             //Create label, textfield and center their text.
             helpText = new JTextPane();
-            helpText.setText("WARNING: THESE SETTINGS MAY GIVE DATALOSS IF NOT USED CORRECTLY \n" +
-                    "If unsure, leave the default settings");
+            helpText.setText("WARNING: THESE SETTINGS MAY GIVE DATALOSS IF NOT USED CORRECTLY");
             helpText.setEnabled(false);
             helpText.setAlignmentX(100);
             helpText.setDisabledTextColor(Color.red);
@@ -52,68 +52,75 @@ import java.sql.SQLException;
             add(helpText);
 
             //Checkboxes
-            checkTables = new JCheckBox("Create tables");
+            checkTables = new JCheckBox("Create/fix DB structure (data safe)");
+            checkTables.setToolTipText("Creates missing tables/views required by this software");
             checkTables.setSelected(true);
-            checkOverWriteTables = new JCheckBox("Overwrite tables");
+
+            checkOverWriteTables = new JCheckBox("Drop all data and create DB structure");
+            checkOverWriteTables.setToolTipText("Drops any existing tables/views, then creates DB structure");
             checkOverWriteTables.setSelected(false);
-            checkViews = new JCheckBox("Create views");
-            checkViews.setSelected(true);
-            checkData = new JCheckBox("Create testdata");
+
+            checkRandomData = new JCheckBox("Fill DB with random data");
+            checkRandomData.setToolTipText("Adds random entries, for testing purposes");
+            checkRandomData.setSelected(false);
+
             add(checkTables);
-            add(checkViews);
-            add(checkData);
             add(checkOverWriteTables);
+            add(checkRandomData);
 
-            JButton buttonExecute = new JButton("Execute");
-            buttonExecute.addActionListener((pressed) -> executeCheckedScripts());
-            add(buttonExecute);
+            //buttonpanel
+            JButton executeButton = new JButton("Execute");
+            executeButton.addActionListener((pressed) -> executeCheckedScripts());
+            executeButton.setToolTipText("Execute selected SQL Scripts");
+            JButton closeButton = new JButton("Close");
+            closeButton.addActionListener((pressed) -> dispose());
+            closeButton.setToolTipText("Close this window");
 
-            //exitButton.addActionListener((pressed) -> dispose());
+            JPanel buttonPanel = new JPanel();
+            buttonPanel.setLayout(new FlowLayout());
+            buttonPanel.add(closeButton);
+            buttonPanel.add(executeButton);
 
-
-
+            add(buttonPanel);
         }
 
     private void executeCheckedScripts(){
         SQL sql = new SQL();
         try {
             sql.connection.setAutoCommit(false);
-            if(checkTables.isSelected()) {
+            if (checkTables.isSelected()) {
                 String path;
-                if(checkOverWriteTables.isSelected()){
+                if (checkOverWriteTables.isSelected()) {
                     path = "sqlscripts/tables.sql";
                     reset.resetDatabaseWithScript(path);
-                }else{
+                } else {
                     path = "sqlscripts/tables_nooverwrite.sql";
                     reset.resetDatabaseWithScript(path);
                 }
-            }
-            if(checkViews.isSelected()) {
-                String path = "sqlscripts/views.sql";
+                path = "sqlscripts/views.sql";
                 reset.resetDatabaseWithScript(path);
             }
-            if(checkData.isSelected()) {
+            if (checkRandomData.isSelected()) {
                 String path = "sqlscripts/randomdata.sql";
                 reset.resetDatabaseWithScript(path);
             }
             sql.connection.commit();
-            helpText.setDisabledTextColor(new Color(0,102,0));
-            helpText.setText("Database operation applied successfully, please close this window in the upper right corner.");
-
+            helpText.setDisabledTextColor(new Color(0, 102, 0));
+            helpText.setText("Database operation successful.");
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
         } catch (SQLException e) {
             try {
-                JOptionPane.showMessageDialog(null, e.toString());
                 sql.connection.rollback();
             } catch (SQLException e1) {
                 JOptionPane.showMessageDialog(null, "Could not rollback, fatal error. System shutdown!");
-                e.printStackTrace();
                 System.exit(-2);
             }
         }finally {
             try {
                 sql.connection.setAutoCommit(true);
             } catch (SQLException e) {
-                JOptionPane.showMessageDialog(null, "Could not set AutoCommit back, do this manually");
+                JOptionPane.showMessageDialog(null, "Could not set AutoCommit back, do this manually!");
             }
         }
     }
