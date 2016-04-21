0946b80a1b8fe93649b9b62fdf5da090bbc4318e
bahafeld <bahafeld@outlook.com>
2016-04-21 17:03:09 +0200

    Added empty missing testclasses

---
+++ src/test/java/backend/StatisticsTest.java
@@ -0,0 +1,104 @@
+package backend;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bahafeld on 21.04.2016...
+ * For FoodPlease
+ */
+public class StatisticsTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getIngredientsInAllOrders() throws Exception {
+
+    }
+
+    @Test
+    public void getTotalSubscriptions() throws Exception {
+
+    }
+
+    @Test
+    public void getTotalOrders() throws Exception {
+
+    }
+
+    @Test
+    public void getOrdersPerDay() throws Exception {
+
+    }
+
+    @Test
+    public void getOrdersPerMonth() throws Exception {
+
+    }
+
+    @Test
+    public void getOrdersAt() throws Exception {
+
+    }
+
+    @Test
+    public void getDeliveriesToday() throws Exception {
+
+    }
+
+    @Test
+    public void getAvgOrdersPerMonthThisYear() throws Exception {
+
+    }
+
+    @Test
+    public void getAllTimePopularIngredient() throws Exception {
+
+    }
+
+    @Test
+    public void getMonthlyPopularIngredient() throws Exception {
+
+    }
+
+    @Test
+    public void getMonthlyPopularFood() throws Exception {
+
+    }
+
+    @Test
+    public void getAllTimePopularFood() throws Exception {
+
+    }
+
+    @Test
+    public void getGrossIncome() throws Exception {
+
+    }
+
+    @Test
+    public void getNumberOfCustomers() throws Exception {
+
+    }
+
+    @Test
+    public void getBiggestCustomer() throws Exception {
+
+    }
+
+    @Test
+    public void getBiggestCustomerThisMonth() throws Exception {
+
+    }
+
+}
